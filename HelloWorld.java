3e589679f018123c7a186d9fd8fa572bcd0c2b1b
dhanubharathi10 <92085125+dhanubharathi10@users.noreply.github.com>
2022-02-02 16:22:12 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld{
 	public static void main(String[] args){
 		for(int i=0;i<5;i++){
-			System.out.println("Hello world.... master..."+i);
+			System.out.println("Hello world.... master... hai"+i);
 		}
 	}
 }
